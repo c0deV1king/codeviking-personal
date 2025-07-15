df6776cff1a67d6204a06666fd82728bff91501a
austin <austinjhiggins@pm.me>
2025-07-15 13:27:58 -0230

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/stylesheet.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-red-600 font-extrabold text-center mt-5">my personal portfolio wow</h1>
+        <p className='italic text-center'>Currently under construction, I'm cool so this site will be cool</p>
+      </div>
+    </>
+  )
+}
+
+export default App
